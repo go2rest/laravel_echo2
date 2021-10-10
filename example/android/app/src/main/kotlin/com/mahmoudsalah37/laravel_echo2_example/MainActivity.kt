3019614c33d80f50cc6bb2433819958fc47fa60b
Mahmoud salah eldien saber <31797198+mahmoudsalah37@users.noreply.github.com>
2021-10-10 15:31:19 +0200

    first commit

---
+++ example/android/app/src/main/kotlin/com/mahmoudsalah37/laravel_echo2_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mahmoudsalah37.laravel_echo2_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <laravel_echo2/laravel_echo2_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  LaravelEcho2PluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("LaravelEcho2Plugin"));
+}
